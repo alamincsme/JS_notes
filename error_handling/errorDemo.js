3d922975aa94d06c902b6ebd2c2f083f23786d1d
alamincsme <alamincsme@gmail.com>
2024-08-27 22:21:05 +0600

    fix issue

---
+++ error_handling/errorDemo.js
@@ -0,0 +1,21 @@
+function divide(a, b) {
+    return a / b;
+}
+
+function main() {
+    let a = 1;
+    let b = 0;
+
+    let result = 0;
+
+    try {
+        result = divide(a, b);
+        if (!isFinite(result)) {
+            throw new Error("Division by zero");
+        }
+    } catch (e) {
+        console.log("You can't divide " + a + " by " + b);
+    }
+}
+
+main();
