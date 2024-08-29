6197baab7fbe31325e55f36911ed708f66fbf77f
alamincsme <alamincsme@gmail.com>
2024-08-29 23:23:19 +0600

    arra in js

---
+++ JS_arra/arra.js
@@ -0,0 +1,12 @@
+let scores = [2, 3, 4, 5, 6, 7, 9 ];
+console.log(scores);//print scores 
+scores.push(10) //add one or more element to end of arra. 
+console.log(scores) ; 
+
+scores.unshift(0) // add element to font of arra. 
+console.log(scores)  
+
+console.log(`len of arra : ${scores.length}`)
+
+
+
