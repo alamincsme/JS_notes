30d0b96eb15cdfd03d320f03058ebda226cc5cab
Al Amin <alamincsme@gmail.com>
2024-08-31 23:39:14 +0600

    learn arrow func

--- JS_arrow_function/sample.js
+++ JS_arrow_function/sample.js
@@ -1,5 +1,7 @@
 
-//arrow function like as lamda function
-let add = (a , b) =>  a + b ; 
+// arrow function like as lamda function
+let add = (a , b) =>  a + b ;  // function (a , b) { return a + b ;}
 console.log(add(20,40))
+
+
 
